25b5a69956e01d04d999b0cfb1fd3ea7da3b2cae
tmdals3000 <tmdals3000@naver.com>
2024-08-05 11:54:44 +0900

    tab 뷰 위치 변경 및 서브1과 서브2 화면 생성

--- app/src/main/java/com/example/ui_main/Sub1Activity.kt
+++ app/src/main/java/com/example/ui_main/Sub1Activity.kt
@@ -1,39 +1,39 @@
 package com.example.ui_main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ui_main.R
 import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
-
+class Sub1Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sub1)
 
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 when (tab.position) {
                     0 -> {
-                        tabLayout.setBackgroundResource(R.color.tab_selected_background)
-                    }
-                    1 -> {
-                        val intent = Intent(this@MainActivity, Sub1Activity::class.java)
+                        val intent = Intent(this@Sub1Activity, MainActivity::class.java)
                         startActivity(intent)
                     }
+                    1 -> {
+                        recreate()
+                    }
                     2 -> {
-                        val intent = Intent(this@MainActivity, Sub2Activity::class.java)
+                        // "서브 2" 탭이 선택되었을 때 다른 액티비티로 이동 (예: Sub2Activity)
+                        val intent = Intent(this@Sub1Activity, Sub2Activity::class.java)
                         startActivity(intent)
                     }
                 }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
-                if (tab.position == 0) {
-                    tabLayout.setBackgroundResource(R.color.tab_unselected_background)
-                }
+                // 선택 해제 시 처리할 내용
             }
 
             override fun onTabReselected(tab: TabLayout.Tab) {
--- app/src/main/java/com/example/ui_main/Sub2Activity.kt
+++ app/src/main/java/com/example/ui_main/Sub2Activity.kt
@@ -1,39 +1,39 @@
 package com.example.ui_main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
-
+class Sub2Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_sub1)
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
+
                 when (tab.position) {
                     0 -> {
-                        tabLayout.setBackgroundResource(R.color.tab_selected_background)
+                        val intent = Intent(this@Sub2Activity, MainActivity::class.java)
+                        startActivity(intent)
                     }
                     1 -> {
-                        val intent = Intent(this@MainActivity, Sub1Activity::class.java)
+                        // "서브 1" 탭이 선택되었을 때 Sub1Activity로 이동
+                        val intent = Intent(this@Sub2Activity, Sub1Activity::class.java)
                         startActivity(intent)
                     }
                     2 -> {
-                        val intent = Intent(this@MainActivity, Sub2Activity::class.java)
-                        startActivity(intent)
+                        // "서브 2" 탭이 선택되었을 때 다른 액티비티로 이동 (예: Sub2Activity)
+                        recreate()
                     }
                 }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
-                if (tab.position == 0) {
-                    tabLayout.setBackgroundResource(R.color.tab_unselected_background)
-                }
+                // 선택 해제 시 처리할 내용
             }
 
             override fun onTabReselected(tab: TabLayout.Tab) {
